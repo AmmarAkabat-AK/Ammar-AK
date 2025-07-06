9a6266c08f13e311e630bf8b61c6ec543e62e897
AmmarAkabat-AK <ammar.akabat@gmail.com>
2025-07-06 05:03:09 +0300

    Add files via upload

---
+++ Hotspot_AK12025/config/lamasat.js
@@ -0,0 +1,82 @@
+﻿Config({
+    "network-name": " برق  نت اللاسلكية",
+    "service-number": "783580750",
+    "popup-adv-time": 5,
+    "popup-adv-img-type": true,
+    "popup-adv-repeat-after": 120,
+    "login-type": "User",
+    "login-chap": 0,
+    "news-line": " مرحباً بكم على  برق  نت اللاسلكية لتغيير السرعات يرجى الدخول الى المتصفح وكتابة a.net ",
+    "input-type": "tel",
+    "input-autocomplete": "on",
+    "speed-trychange": 100 ,//عدد مرات هلاح التغيير
+    "speed-trychange-timeout": 1 ,// مدة فترة  حضر التغيير بالدقائق
+    "input-rm-white-spaces": 0,
+    "input-to-lower": 0,
+    "input-to-upper": 0,
+    "input-to-arabic-numbers": 1,
+    "input-only-numbers": 0,
+    "input-no-numbers": 0,
+    "input-only-alphanumeric": 0,
+    "input-to-tel-type-when": 0,
+    "enable-hot-cookie": 1,
+    "enable-hot-blocker": 1,
+    "clear-router-cookie": 1,
+    "clear-hot-cookie": 1,
+    "block-time": 1,
+    "try-count": 20,
+    "warn-when": 10,
+    "warn-message": "تحذير !! عدد محاولاتك الخاطئة اصبح {{tryCounter}} محاولات, عدد المحاولات المسموح بها هي {{tryCount}} محاولات فقط, عدد محاولاتك المتبقية {{restTryCount}} محاولات, سيتم حظرك لمدة {{blockTime}} دقائق اذا تجاوزت العدد المسموح للمحاولات",
+    "price-button": true,
+    "sell-point-button": true,
+    "app-store-status-button": false,
+    "show-date-field": false,
+    "loan-button": true,
+    "login-speeds-mode": true,
+    "redirect-to-esterahah": "http://10.10.10.10",
+    "redirect-to-mobasher": "http://10.10.10.10/liveStream/",
+    "app-store-base-url": "",
+    "app-store-base-url-ext": "",
+   "profiles": [
+  
+		 
+		{
+            "price": "100 ريال",
+            "time": "5 ساعات",
+            "transfer": "500 ميجا بايت",
+            "validity": "5 ايام",
+        },
+		
+       
+		 {
+            "price": "250 ريال",
+            "time": "10 ساعة",
+            "transfer": "1024 ميجا بايت",
+            "validity": "10 أيام",
+        },
+		
+		
+		 
+		
+	
+        
+    ],
+    "sell-points": [
+        { "name": "جميع البقالات الموجودة في اماكن التغطية   " },
+        { "name": "  جديد  " },
+        { "name": "  جديد  " },
+      
+        
+    ]
+})
+
+
+
+
+
+
+
+
+
+
+
